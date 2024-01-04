f1c235e8670923e74fbe0269268587d6297f6c56
tantality <angel.tretyak@mail.ru>
2024-01-04 13:31:38 +0300

    feat: add an app config

--- apps/gateway-api/src/app.module.ts
+++ apps/gateway-api/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'api/app/auth/auth.module';
+import appConfig from 'api/config/app.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'development.env',
+      load: [appConfig],
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- apps/gateway-api/src/main.ts
+++ apps/gateway-api/src/main.ts
@@ -1,9 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3001);
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('app.port');
+  await app.listen(port, () => console.log(`Server started at port: ${port}`));
 }
 bootstrap();
