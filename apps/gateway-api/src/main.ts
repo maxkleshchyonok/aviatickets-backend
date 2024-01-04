0024030da8d96ba4d10d8f609a52ab2459e4456f
tantality <angel.tretyak@mail.ru>
2024-01-05 01:38:23 +0300

    feat: set a global validation pipe

--- apps/gateway-api/src/main.ts
+++ apps/gateway-api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const config = app.get(ConfigService);
   const port = config.get<number>('app.port');
