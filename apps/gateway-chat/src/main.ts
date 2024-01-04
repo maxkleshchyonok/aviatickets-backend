af2a013c47f2ee6f5c4f508a07ce25873f82dae8
ilik-sh <i.shyshparonak@gmail.com>
2024-01-04 16:25:23 +0300

    feat: specify RedisAdapter as app WebSocketAdapter

--- apps/gateway-chat/src/main.ts
+++ apps/gateway-chat/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayChatModule } from './gateway-chat.module';
+import { RedisIoAdapter } from 'apps/gateway-chat/src/adapters/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayChatModule);
+  const redisIoAdapter = new RedisIoAdapter(app)
+  await redisIoAdapter.connectToRedis()
+  app.useWebSocketAdapter(redisIoAdapter)
   await app.listen(3000);
+  
 }
 bootstrap();
