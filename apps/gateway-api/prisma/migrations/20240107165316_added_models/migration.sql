b59bf897707bb7c777ea8568e20d2b3b37aaf874
tantality <angel.tretyak@mail.ru>
2024-01-07 19:45:22 +0300

    feat: update prisma models

--- apps/gateway-api/prisma/migrations/20240107165316_added_models/migration.sql
+++ apps/gateway-api/prisma/migrations/20240107165316_added_models/migration.sql
@@ -4,6 +4,15 @@ CREATE TYPE "UserPermissions" AS ENUM ('permissions.all');
 -- CreateEnum
 CREATE TYPE "RoleTypes" AS ENUM ('admin', 'user', 'sales');
 
+-- CreateEnum
+CREATE TYPE "FlightStatuses" AS ENUM ('planned', 'completed');
+
+-- CreateEnum
+CREATE TYPE "TicketTypes" AS ENUM ('economy', 'business');
+
+-- CreateEnum
+CREATE TYPE "Cities" AS ENUM ('Kabul', 'Tirana', 'Algiers', 'Andorra la Vella', 'Luanda', 'Buenos Aires', 'Yerevan', 'Canberra', 'Vienna', 'Baku', 'Nassau', 'Manama', 'Dhaka', 'Bridgetown', 'Minsk', 'Brussels', 'Belmopan', 'Thimphu', 'La Paz', 'Sarajevo', 'Gaborone', 'Bandar Seri Begawan', 'Sofia', 'Ouagadougou', 'Gitega', 'Phnom Penh', 'Yaounde', 'Ottawa', 'Praia', 'Bangui', 'Santiago', 'Beijing', 'Bogota', 'Moroni', 'Brazzaville', 'Kinshasa', 'San Jose', 'Yamoussoukro', 'Zagreb', 'Havana', 'Nicosia', 'Prague', 'Copenhagen', 'Djibouti', 'Roseau', 'Santo domingo', 'Dili', 'Quito', 'Cairo', 'San Salvador', 'Malabo', 'Asmara', 'Tallinn', 'Addis Ababa', 'Suva', 'Helsinki', 'Paris', 'Libreville', 'Banjul', 'Tbilisi', 'Berlin', 'Accra', 'Athens', 'Saint Georges', 'Conakry', 'Bissau', 'Georgetown', 'Tegucigalpa', 'Budapest', 'Antananarivo', 'Lilongwe', 'Kuala Lumpur', 'Male', 'Bamako', 'Valletta', 'Majuro', 'Nouakchott', 'Palikir', 'Chisinau', 'Monaco', 'Ulaanbaatar', 'Podgorica', 'Rabat', 'Maputo', 'Rangoon', 'Windhoek', 'Kathmandu', 'Amsterdam', 'Wellington', 'Managua', 'Niamey', 'Abuja', 'Oslo', 'Muscat', 'Islamabad', 'Melekeok', 'Port Moresby', 'Asuncion', 'Lima', 'Manila', 'Warsaw', 'Lisbon', 'Doha', 'Bucharest', 'Moscow', 'Kigali', 'Basseterre', 'Castries', 'Kingstown', 'Apia', 'San Marino');
+
 -- CreateTable
 CREATE TABLE "roles" (
     "id" UUID NOT NULL,
@@ -26,7 +35,7 @@ CREATE TABLE "users" (
     "role_type" "RoleTypes" NOT NULL DEFAULT 'user',
     "password" TEXT NOT NULL,
     "refresh_token" TEXT,
-    "roleId" UUID NOT NULL,
+    "role_id" UUID NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +47,7 @@ CREATE TABLE "tickets" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "seats_stock" INTEGER NOT NULL,
     "price" INTEGER NOT NULL,
-    "type" TEXT NOT NULL,
+    "type" "TicketTypes" NOT NULL,
     "flight_id" UUID NOT NULL,
 
     CONSTRAINT "tickets_pkey" PRIMARY KEY ("id")
@@ -63,7 +72,7 @@ CREATE TABLE "passengers" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
-    "passport" TEXT NOT NULL,
+    "passport_id" TEXT NOT NULL,
     "booking_id" UUID NOT NULL,
 
     CONSTRAINT "passengers_pkey" PRIMARY KEY ("id")
@@ -74,11 +83,11 @@ CREATE TABLE "flights" (
     "id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "origin" TEXT NOT NULL,
-    "destination" TEXT NOT NULL,
-    "departure" TIMESTAMP(3) NOT NULL,
-    "arrival" TIMESTAMP(3) NOT NULL,
-    "status" TEXT NOT NULL,
+    "origin_city" "Cities" NOT NULL,
+    "destination_city" "Cities" NOT NULL,
+    "departure_time" TIMESTAMP(3) NOT NULL,
+    "arrival_time" TIMESTAMP(3) NOT NULL,
+    "status" "FlightStatuses" NOT NULL,
     "price" INTEGER NOT NULL,
     "plane_id" UUID NOT NULL,
 
@@ -92,7 +101,6 @@ CREATE TABLE "planes" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
     "capacity" INTEGER NOT NULL,
-    "image" TEXT NOT NULL,
     "carrier" TEXT NOT NULL,
 
     CONSTRAINT "planes_pkey" PRIMARY KEY ("id")
@@ -105,10 +113,10 @@ CREATE UNIQUE INDEX "roles_id_type_key" ON "roles"("id", "type");
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "passengers_passport_key" ON "passengers"("passport");
+CREATE UNIQUE INDEX "passengers_passport_id_key" ON "passengers"("passport_id");
 
 -- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "tickets" ADD CONSTRAINT "tickets_flight_id_fkey" FOREIGN KEY ("flight_id") REFERENCES "flights"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
