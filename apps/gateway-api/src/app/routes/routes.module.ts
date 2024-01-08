d2d607d410bf29d8c72d6c775d15cd18c4b318a1
tantality <angel.tretyak@mail.ru>
2024-01-08 20:56:21 +0300

    feat: implement a one-way route search

--- apps/gateway-api/src/app/routes/routes.module.ts
+++ apps/gateway-api/src/app/routes/routes.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { FlightGraphModule } from 'api/libs/flight-graph/flight-graph.module';
 import { PrismaModule } from 'api/libs/prisma/prisma.module';
 import { RoutesController } from './routes.controller';
 import { RoutesService } from './routes.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FlightGraphModule],
   controllers: [RoutesController],
   providers: [RoutesService],
 })
--- apps/gateway-api/src/app/routes/routes.service.ts
+++ apps/gateway-api/src/app/routes/routes.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { RouteDto } from 'api/domain/dto/route.dto';
+import { RoutesDto } from 'api/domain/dto/routes.dto';
+import { FlightGraphService } from 'api/libs/flight-graph/flight-graph.service';
 import { GetRoutesQueryDto } from './domain/get-routes-query.dto';
 
 @Injectable()
 export class RoutesService {
-  async findRoutes(query: GetRoutesQueryDto) {}
+  constructor(private flightGraph: FlightGraphService) {}
+
+  async findRoutes(query: GetRoutesQueryDto) {
+    const toDestinationRoutes = this.flightGraph.findRoutes(
+      query.originCity,
+      query.destinationCity,
+    );
+
+    const toDestinationRoutesDto = RouteDto.fromRoutes(toDestinationRoutes);
+
+    return RoutesDto.fromRoutes(toDestinationRoutesDto);
+  }
 }
