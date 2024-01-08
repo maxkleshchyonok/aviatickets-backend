8d884f7f5ba4933b975e3eaf81a10173975e7e65
tantality <angel.tretyak@mail.ru>
2024-01-08 18:27:14 +0300

    feat: implement a flight graph

---
+++ apps/gateway-api/src/flight-graph/index.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { Flight } from '@prisma/client';
+import { Route } from 'api/types/route.type';
+
+@Injectable()
+export class FlightGraph {
+  vertices: Map<Flight, Flight[]>;
+
+  constructor() {
+    this.vertices = new Map<Flight, Flight[]>();
+  }
+
+  addVertex(vertex: Flight) {
+    if (!this.vertices.get(vertex)) {
+      this.vertices.set(vertex, []);
+    }
+  }
+
+  addEdge(vertex1: Flight, vertex2: Flight) {
+    this.vertices.get(vertex1).push(vertex2);
+  }
+
+  findRoutes(originCity, destinationCity) {
+    const routes: Route[] = [];
+    const visited = new Set();
+
+    const flightVertices = Array.from<Flight>(this.vertices.keys());
+
+    function dfs(city, route, arrivalTime) {
+      visited.add(city);
+
+      if (city === destinationCity) {
+        routes.push(route.slice());
+      } else {
+        const flightsFromCurrentCity = flightVertices.filter(
+          (flight) => flight.originCity === city,
+        );
+
+        for (const flight of flightsFromCurrentCity) {
+          if (
+            !visited.has(flight.destinationCity) &&
+            flight.departureTime >= arrivalTime
+          ) {
+            const newRoute = route.slice();
+            newRoute.push(flight);
+            dfs(flight.destinationCity, newRoute, flight.arrivalTime);
+          }
+        }
+      }
+
+      visited.delete(city);
+    }
+
+    const flightsFromOriginCity = flightVertices.filter(
+      (flight) => flight.originCity === originCity,
+    );
+
+    for (const flight of flightsFromOriginCity) {
+      dfs(flight.destinationCity, [flight], flight.arrivalTime);
+    }
+
+    return routes;
+  }
+}
