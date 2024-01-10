a2fd4dd716258438b463802be62a8ee3764ac616
tantality <angel.tretyak@mail.ru>
2024-01-10 12:45:24 +0300

    feat: add dates to a dto

--- apps/gateway-api/src/app/routes/domain/get-routes-query.dto.ts
+++ apps/gateway-api/src/app/routes/domain/get-routes-query.dto.ts
@@ -1,6 +1,13 @@
 import { Cities } from '@prisma/client';
 import { RemoveExtraSpaces } from 'api/decorators/remove-extra-spaces.decorator';
-import { IsEnum } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  MinDate,
+} from 'class-validator';
 
 export class GetRoutesQueryDto {
   @IsEnum(Cities)
@@ -10,4 +17,20 @@ export class GetRoutesQueryDto {
   @IsEnum(Cities)
   @RemoveExtraSpaces()
   destinationCity: Cities;
+
+  @IsNotEmpty()
+  @Transform(({ value }) => new Date(value))
+  @IsDate()
+  @MinDate(new Date())
+  @Type(() => Date)
+  @RemoveExtraSpaces()
+  departureDate: Date;
+
+  @IsOptional()
+  @Transform(({ value }) => value && new Date(value))
+  @IsDate()
+  @MinDate(new Date())
+  @Type(() => Date)
+  @RemoveExtraSpaces()
+  arrivalDate?: Date;
 }
