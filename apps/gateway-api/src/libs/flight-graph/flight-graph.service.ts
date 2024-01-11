ed049248c9d86d5f7dd48176976675d9ce8a1b24
tantality <angel.tretyak@mail.ru>
2024-01-11 19:29:14 +0300

    refactor: change find-routes method

--- apps/gateway-api/src/libs/flight-graph/flight-graph.service.ts
+++ apps/gateway-api/src/libs/flight-graph/flight-graph.service.ts
@@ -9,6 +9,19 @@ interface FindRouteOptions {
   passengerAmount: number;
 }
 
+interface FindRouteDfs {
+  currentCity: string;
+  route: Flight[];
+  previousFlightArrivalTime: Date;
+  visitedCities: Set<string>;
+  routes: Route[];
+}
+
+interface FindFlightConditions {
+  destinationCity: string;
+  passengerAmount: number;
+}
+
 @Injectable()
 export class FlightGraphService {
   vertices: Map<Flight, Flight[]>;
@@ -53,50 +66,87 @@ export class FlightGraphService {
       const { originCity, destinationCity, departureTime, passengerAmount } =
         options;
       const routes: Route[] = [];
-      const visited = new Set<string>();
-
+      const visitedCities = new Set<string>();
       const flightVertices = Array.from<Flight>(this.vertices.keys());
 
-      function dfs(city: string, route: Flight[], arrivalTime: Date) {
-        visited.add(city);
-
-        if (city === destinationCity) {
-          routes.push(route.slice());
-        } else {
-          const flightsFromCurrentCity = flightVertices.filter(
-            (flight) =>
-              flight.originCity === city &&
-              flight.availableSeatAmount >= passengerAmount,
-          );
-
-          for (const flight of flightsFromCurrentCity) {
-            if (
-              !visited.has(flight.destinationCity) &&
-              flight.departureTime >= arrivalTime
-            ) {
-              const newRoute = route.slice();
-              newRoute.push(flight);
-              dfs(flight.destinationCity, newRoute, flight.arrivalTime);
-            }
-          }
-        }
-
-        visited.delete(city);
-      }
-
-      const flightsFromOriginCity = flightVertices.filter(
-        (flight) =>
-          flight.originCity === originCity &&
+      const flightsFromOriginCity = flightVertices.filter((flight) => {
+        const areOriginCitiesEqual = flight.originCity === originCity;
+        const areDepartureDatesEqual =
           flight.departureTime.toLocaleDateString() ===
-            departureTime.toLocaleDateString() &&
-          flight.availableSeatAmount >= passengerAmount,
-      );
+          departureTime.toLocaleDateString();
+        const isThereNecessarySeatAmount =
+          flight.availableSeatAmount >= passengerAmount;
+
+        if (
+          areOriginCitiesEqual &&
+          areDepartureDatesEqual &&
+          isThereNecessarySeatAmount
+        ) {
+          return flight;
+        }
+      });
 
       for (const flight of flightsFromOriginCity) {
-        dfs(flight.destinationCity, [flight], flight.arrivalTime);
+        const data: FindRouteDfs = {
+          currentCity: flight.destinationCity,
+          route: [flight],
+          previousFlightArrivalTime: flight.arrivalTime,
+          visitedCities,
+          routes,
+        };
+
+        const flightConditions = { destinationCity, passengerAmount };
+        this.dfs(data, flightConditions);
       }
 
       return resolve(routes);
     });
+  }
+
+  private dfs(data: FindRouteDfs, flightConditions: FindFlightConditions) {
+    const {
+      currentCity,
+      route,
+      previousFlightArrivalTime,
+      visitedCities,
+      routes,
+    } = data;
+
+    visitedCities.add(currentCity);
+
+    if (currentCity === flightConditions.destinationCity) {
+      routes.push(route.slice());
+      visitedCities.delete(currentCity);
+      return;
+    }
+
+    const flightsFromCurrentCity = Array.from<Flight>(
+      this.vertices.keys(),
+    ).filter(
+      (flight) =>
+        flight.originCity === currentCity &&
+        flight.availableSeatAmount >= flightConditions.passengerAmount,
+    );
+
+    for (const flight of flightsFromCurrentCity) {
+      if (
+        !visitedCities.has(flight.destinationCity) &&
+        flight.departureTime >= previousFlightArrivalTime
+      ) {
+        const newRoute = route.slice();
+        newRoute.push(flight);
+
+        const data: FindRouteDfs = {
+          visitedCities,
+          routes,
+          route: newRoute,
+          currentCity: flight.destinationCity,
+          previousFlightArrivalTime: flight.arrivalTime,
+        };
+        this.dfs(data, flightConditions);
+      }
+    }
+
+    visitedCities.delete(currentCity);
   }
 }
