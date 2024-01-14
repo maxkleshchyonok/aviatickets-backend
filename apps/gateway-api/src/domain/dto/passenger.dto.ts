9213e792a6d24206341fedb106d2d4506d213f4e
tantality <angel.tretyak@mail.ru>
2024-01-14 17:50:50 +0300

    feat: add dtos

---
+++ apps/gateway-api/src/domain/dto/passenger.dto.ts
@@ -0,0 +1,31 @@
+import { Passenger } from '@prisma/client';
+import { UUIDDto } from './uuid.dto';
+
+export class PassengerDto extends UUIDDto {
+  firstName: string;
+  lastName: string;
+  passportId: string;
+
+  static fromEntity(entity?: Passenger) {
+    if (!entity) {
+      return;
+    }
+
+    const it = new PassengerDto();
+    it.id = entity.id;
+    it.createdAt = entity.createdAt.valueOf();
+    it.updatedAt = entity.updatedAt.valueOf();
+    it.firstName = entity.firstName;
+    it.lastName = entity.lastName;
+    it.passportId = entity.passportId;
+    return it;
+  }
+
+  static fromEntities(entities?: Passenger[]) {
+    if (!entities?.map) {
+      return;
+    }
+
+    return entities.map((entity) => this.fromEntity(entity));
+  }
+}
