a1aae3f375a7e3f109a3a7b141564a7899c73e77
tantality <angel.tretyak@mail.ru>
2024-01-14 18:15:16 +0300

    feat: add obtaining of all bookings

---
+++ apps/gateway-api/src/app/bookings/domain/get-all-bookings-query.dto.ts
@@ -0,0 +1,3 @@
+import { PaginationQueryDto } from 'api/domain/dto/pagination-query.dto';
+
+export class GetAllBookingsQueryDto extends PaginationQueryDto {}
--- apps/gateway-api/src/domain/repos/bookings.repo.ts
+++ apps/gateway-api/src/domain/repos/bookings.repo.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { GetAllBookingsQueryDto } from 'api/app/bookings/domain/get-all-bookings-query.dto';
 import { PrismaService } from 'api/libs/prisma/prisma.service';
 
 @Injectable()
 export class BookingsRepo {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findAllBookings(query: GetAllBookingsQueryDto) {
+    const { pageNumber, pageSize } = query;
+
+    const prismaQuery: Prisma.BookingFindManyArgs = {
+      skip: (pageNumber - 1) * pageSize,
+      take: pageSize,
+      include: {
+        bookingItems: {
+          include: {
+            passenger: true,
+            flight: true,
+          },
+          orderBy: {
+            flightId: Prisma.SortOrder.asc,
+          },
+        },
+        user: true,
+      },
+    };
+
+    const [count, bookings] = await this.prisma.$transaction([
+      this.prisma.booking.count({ where: prismaQuery.where }),
+      this.prisma.booking.findMany(prismaQuery),
+    ]);
+
+    return { count, bookings };
+  }
 }
