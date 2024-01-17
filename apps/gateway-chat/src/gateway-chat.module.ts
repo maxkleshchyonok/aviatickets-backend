47b278e897d96140731af8266193fb43d3ad41f2
ilik-sh <i.shyshparonak@gmail.com>
2024-01-17 18:19:32 +0300

    feat: inject Config- and CacheModule

--- apps/gateway-chat/src/gateway-chat.module.ts
+++ apps/gateway-chat/src/gateway-chat.module.ts
@@ -3,9 +3,34 @@ import { Module } from '@nestjs/common';
 import { GatewayChatController } from 'chat/gateway-chat.controller';
 import { GatewayChatService } from 'chat/gateway-chat.service';
 import { SocketModule } from 'chat/socket/socket.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import redisConfig from 'chat/config/redis.config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [redisConfig],
+      isGlobal: true
+    }),
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        
+        store: redisStore,
+        socket: {
+          host: config.get('redis.host'),
+          port: config.get('redis.port')
+        },
+
+        ttl: config.get('redis.ttl')
+      }),
+      inject: [ConfigService],
+      isGlobal: true,
+    }),
     SocketModule,
   ],
   controllers: [GatewayChatController],
