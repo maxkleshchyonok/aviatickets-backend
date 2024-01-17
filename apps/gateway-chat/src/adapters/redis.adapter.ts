08722e14aded7b40e00f91e2706553bfe6cdc3b5
ilik-sh <i.shyshparonak@gmail.com>
2024-01-17 17:37:46 +0300

    feat: create redis adapter

--- apps/gateway-chat/src/adapters/redis.adapter.ts
+++ apps/gateway-chat/src/adapters/redis.adapter.ts
@@ -2,6 +2,9 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
+const jwt = require('jsonwebtoken')
+import { jwtMiddleware } from 'chat/middlewares/jwt.middleware';
+
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
@@ -17,6 +20,7 @@ export class RedisIoAdapter extends IoAdapter {
 
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
+    server.use(jwtMiddleware)
     server.adapter(this.adapterConstructor);
     return server;
   }
