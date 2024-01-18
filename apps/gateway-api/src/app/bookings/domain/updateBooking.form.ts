0d18843cca8f025e00f6c033891d226808546b6b
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2024-01-18 15:10:23 +0300

    feat: add update booking

---
+++ apps/gateway-api/src/app/bookings/domain/updateBooking.form.ts
@@ -0,0 +1,9 @@
+import { BookingStatuses } from "@prisma/client";
+import { RemoveExtraSpaces } from "api/decorators/remove-extra-spaces.decorator";
+import { IsEnum } from "class-validator";
+
+export class UpdateBookingForm {
+    @IsEnum(BookingStatuses) 
+    @RemoveExtraSpaces()
+    status: BookingStatuses;
+  }
