3727dc70ce42be2f68114faa2894a9c33f568d3c
tantality <angel.tretyak@mail.ru>
2024-01-19 21:04:33 +0300

    refactor: move a dto to another file

--- apps/gateway-api/src/domain/dto/tickets.dto.ts
+++ apps/gateway-api/src/domain/dto/tickets.dto.ts
@@ -1,52 +1,7 @@
 import { JourneyTypes } from 'api/enums/journey-types.enum';
 import { PaginationQueryDto } from './pagination-query.dto';
 import { RouteDto } from './route.dto';
-import { v4 } from 'uuid';
-
-class TicketDto {
-  id: string;
-  price: number;
-  toDestinationRoute: RouteDto;
-  toOriginRoute: RouteDto | null;
-
-  static toTicket(
-    toDestinationRoute: RouteDto,
-    toOriginRoute: RouteDto | null = null,
-  ) {
-    const it = new TicketDto();
-    it.id = v4();
-    it.toDestinationRoute = toDestinationRoute;
-    it.toOriginRoute = toOriginRoute;
-    it.price =
-      toDestinationRoute.price + (toOriginRoute ? toOriginRoute.price : 0);
-
-    return it;
-  }
-
-  static fromRoutes(
-    toDestinationRoutes: RouteDto[],
-    toOriginRoutes: RouteDto[] = [],
-  ) {
-    const areThereOriginRoutes = Boolean(toOriginRoutes.length);
-
-    if (!toDestinationRoutes.length) {
-      return [];
-    }
-
-    const tickets = toDestinationRoutes
-      .map((toDestinationRoute) => {
-        if (areThereOriginRoutes) {
-          return toOriginRoutes.map((toOriginRoute) =>
-            this.toTicket(toDestinationRoute, toOriginRoute),
-          );
-        }
-        return this.toTicket(toDestinationRoute) as TicketDto;
-      })
-      .flat(1);
-
-    return tickets;
-  }
-}
+import { TicketDto } from './ticket.dto';
 
 export class TicketsDto {
   count: number;
