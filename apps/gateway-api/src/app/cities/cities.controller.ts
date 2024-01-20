d671366c22f5aa95e8fcf44bf0dc1e9a5343d2b2
tantality <angel.tretyak@mail.ru>
2024-01-20 16:37:23 +0300

    feat: implement obtaining of all cities

--- apps/gateway-api/src/app/cities/cities.controller.ts
+++ apps/gateway-api/src/app/cities/cities.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Cities } from '@prisma/client';
 
 @Controller('cities')
-export class CitiesController {}
+export class CitiesController {
+  @Get()
+  async getAllCities() {
+    return Object.values(Cities);
+  }
+}
