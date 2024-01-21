923f67816bb01500be6483c089752833d297b764
ilik-sh <i.shyshparonak@gmail.com>
2024-01-21 11:59:33 +0300

    feat: integrate prisma excception filter in app

--- apps/gateway-api/src/main.ts
+++ apps/gateway-api/src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './filters/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const {httpAdapter} = app.get(HttpAdapterHost)
+  
   app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors({origin: 'http://localhost:3000'});
 
-  const config = app.get(ConfigService);
+  const config = app.get(ConfigService);  
   const port = config.get<number>('app.port');
   await app.listen(port, () => console.log(`Server started at port: ${port}`));
 }
