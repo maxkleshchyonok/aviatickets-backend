7deee1e1ca71cf870ca18953918228d17318f049
ilik-sh <i.shyshparonak@gmail.com>
2024-01-21 11:58:42 +0300

    feat: create prisma exception filter

---
+++ apps/gateway-api/src/filters/prisma-client-exception.filter.ts
@@ -0,0 +1,19 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response
+    .status(500)
+    .json({
+        statusCode: 500,
+        message: 'Internal server error. Try again later'
+    })
+  }
+}
