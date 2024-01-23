60ceba1e993015ea72ab143e43264dce6de46db3
ilik-sh <i.shyshparonak@gmail.com>
2024-01-23 13:54:04 +0300

    fix: fix path error

--- apps/gateway-api/src/domain/repos/flights.repo.ts
+++ apps/gateway-api/src/domain/repos/flights.repo.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { PrismaService } from 'api/libs/prisma/prisma.service';
+import { PrismaService } from 'libs/prisma/prisma.service';
 
 @Injectable()
 export class FlightsRepo {
